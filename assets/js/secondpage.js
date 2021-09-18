797a34cb1937b218f96b69e22d914643c05325d3
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2021-09-18 16:50:36 -0500

    added containers to display weather info

--- assets/js/secondpage.js
+++ assets/js/secondpage.js
@@ -3,7 +3,8 @@ var userInput=document.getElementById("userInput");
 var city = "Austin"
 var query = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" +apiKey;
     
-//      userInput.addEventListener("submit",);
+//add event listener to button click and set the data to pass through the query    
+//  userInput.addEventListener("submit",);
 
 // const el = document.getElementById("outside");
 // el.addEventListener("click", modifyText, false);
@@ -26,6 +27,8 @@ fetch(query)
                 return res.json()
             }
         }).then(function(data){
+            displayCurrentData (data, main);
+            //displayFutureData(data, main)
             console.log(data);
         });
         //icon  where data is an object, dailyis an array call that array at the index (in the loop) with the key weather and will be an arrayand will always be an uneccesary array
@@ -33,3 +36,12 @@ fetch(query)
         //loop over the array dor a five day forecast
 //data.daily[i].weather[0].icon
        });
+
+       //CURRENT WEATHER from response we need to grab "city_name" = lat & lon,  "date"= ,"icon" or something to set and if/else icon per status, "current_weather" (5 days worth so array from 0 to 4) indicating the following:  "humidity", "wind-speed", "UV index" with indicators of (favorable, moderate, severe),
+      
+       var displayCurrentData = (function(query){})
+    
+      
+      
+       //FUTURE WEATHER from response we need to grab "city_name",  "date","icon" or something to set and if/else icon per status, "current_weather" (5 days worth, so array from 0 to 4) indicating the following:  "humidity", "wind-speed", "UV index" with indicators of (favorable, moderate, severe),
+      // var displayFutureData
